adf4502a741f0bcdbe61507028ad368c618f39df
wwhtml <1129942397@qq.com>
2023-12-21 01:30:53 +0800

    aaaaaa

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory /* createWebHashHistory */ } from "vue-router";
 import routes from "./routes";
 
 //引入进度条
@@ -10,8 +10,8 @@ import { useUserStore } from "@/stores/index";
 console.log(import.meta);
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
-  // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHashHistory(),
   routes: routes
 });
 console.log(router);
