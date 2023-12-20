1e05f75e79a5a359def48c770c9f7f8f8ba837a7
wwhtml <1129942397@qq.com>
2023-12-21 01:21:36 +0800

    dddd

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,7 @@ import { useUserStore } from "@/stores/index";
 console.log(import.meta);
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
   history: createWebHashHistory(),
   routes: routes
 });
